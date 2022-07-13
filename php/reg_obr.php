775ae38ac04eaed10cee781cb35940fb5eb96ae3
Chizhikov <57985484+SergioChizhikov@users.noreply.github.com>
2022-07-13 12:36:28 +0300

    Корректировка ответа на фронт

--- php/reg_obr.php
+++ php/reg_obr.php
@@ -3,19 +3,21 @@ header('Content-Type: text/html; charset=utf-8');
 
 $mysqli = mysqli_connect("localhost", "oefnbvtr_0540", "12345", "oefnbvtr_0540");
 if ($mysqli == false) {
-  print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+  print("error");
 } else {
-  print("Соединение установлено успешно");
+  //print("Соединение установлено успешно");
+
+  $name = $_POST['name'];
+  $lastname = $_POST['lastname'];
+  $email = $_POST['email'];
+  $pass = $_POST['pass'];
+
+  $result = $mysqli->query("SELECT * FROM `users` WHERE `email`='$email'");
+
+  if ($result->num_rows != 0) {
+    print("exist");
+  } else {
+    $mysqli->query("INSERT INTO `users`(`name`, `lastname`, `email`, `pass`) VALUES ('$name', '$lastname', '$email', '$pass')");
+    print("ok");
+  }
 }
-
-
-$name = $_POST['name'];
-$lastname = $_POST['lastname'];
-$email = $_POST['email'];
-$pass = $_POST['pass'];
-echo "<hr>";
-echo "Имя: $name<br>
-Фамилия: $lastname<br>
-E-mail: $email<br>
-Пароль: $pass";
-?>
